d1ea3f620d426379ac2b9ba4820632a67ce89ff8
Emanuelly Vitoria <emanuelly.lima@estudante.ifgoiano.edu.br>
2026-09-23 07:48:17 -0300

    arrumei o cad usuario e fiz o salvar usuario
    nao testei ainda rs

---
+++ codigo/usuario/salvar_usuario.php
@@ -0,0 +1,24 @@
+<?php
+require_once "../verifica_sessao.php";
+require_once "../conexao.php";
+
+$id = $_GET['id'];
+$nome = $_POST['nome'];
+$apelido = $_POST['apelido'];
+$email = $_POST['email'];
+$senha = $_POST['senha'];
+$foto = $_POST['foto'];
+$area = $_POST['area'];
+$carga = $_POST['carga'];
+
+if ($id == 0) {
+    $sql = "INSERT INTO usuario (nome, apelido, email, senha, foto) VALUES ('$nome', '$apelido', '$email', '$senha', '$foto')";
+}
+else {
+    $sql = "UPDATE usuario SET nome = '$nome', apelido = '$apelido', email = '$email', senha = '$senha', foto = '$foto' WHERE idusuario = $id";
+}
+
+mysqli_query($conexao, $sql);
+
+header("Location: ../sucesso.html");
+?>
