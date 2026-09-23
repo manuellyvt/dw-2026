0ee277e5145ca58f6d961100ec52e68ea6622f41
otavioa841 <otavio.oliveira1@estudante.ifgoiano.edu.br>
2026-09-23 08:18:18 -0300

    verificação de login

--- codigo/login/verifica_sessao.php
+++ codigo/login/verifica_sessao.php
@@ -1,6 +1,5 @@
 <?php
-    //verificar se o usuario fez login
-    //se SIM vê essa página
+
     session_start();
     if (!isset($_SESSION['email'])) {
 
--- codigo/login/verificar _login.php
+++ codigo/login/verificar _login.php
@@ -1,31 +1,28 @@
 <?php
     require_once "conexao.php";
 
-    //pegar os valores digitados
+
     $email = $_POST['email'];
     $senha = $_POST['senha'];
 
-    //comparar com os do banco: se existe lá no banco
+   
     $sql = "SELECT * FROM usuario WHERE email = '$email' AND senha = '$senha'";
     
     $resultados = mysqli_query($conexao, $sql);
 
-    // echo $resultados;
-    // echo sizeof($resultados);
+    
     $quantidade = mysqli_num_rows($resultados);
 
-    //se existir no banco -> principal.php
-    //se NÃO existe no banco -> index.php
+  
     if ($quantidade == 1) {
 
         $linha = mysqli_fetch_array($resultados);
-        // sessao -> variável global
-        // $_SESSION['logado'] = 1;
+    
         session_start();
         $_SESSION['email'] = $linha['email'];
         $_SESSION['nome'] = $linha['nome'];
         $_SESSION['idusuario'] = $linha['idusuario'];
-        // $linha['foto'];
+    
     }
 
 
